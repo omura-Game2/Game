24f58d9d0614a44d184203978e2ad4b4f0d9196f
omuratakumi <20010501@anabuki-net.ne.jp>
2021-06-14 11:02:46 +0900

    制作途中

--- 2DLv1_99_Game/GameProgramming/CSceneGame.cpp
+++ 2DLv1_99_Game/GameProgramming/CSceneGame.cpp
@@ -19,19 +19,23 @@ void CSceneGame::Init() {
 	//クラスのメンバ変数への代入
 	CPlayer *Player = new CPlayer();
 	Player->x = 150;
-	Player->y = 150;
+	Player->y = -1400;
 	Player->w = 25;
 	Player->h = 25;
 	Player->mEnabled = true;
 
-	int map[15][8] =
+	int map[19][8] =
 	{
-		{ 1, 1, 1, 1, 1, 1, 1, 1},
+		{ 9, 1, 1, 1, 1, 1, 1, 9 },
 		{ 1, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 1 },
-		{ 1, 2, 0, 0, 0, 0, 2, 1 },
+		{ 1, 0, 0, 3, 0, 0, 2, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 1 },
-		{ 1, 0, 0, 2, 0, 0, 0, 1 },
+		{ 1, 0, 0, 0, 0, 0, 2, 1 },
+		{ 1, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 2, 0, 0, 0, 2, 1 },
+		{ 1, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 2, 0, 0, 0, 0, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 1 },
@@ -42,8 +46,7 @@ void CSceneGame::Init() {
 		{ 1, 0, 0, 0, 0, 0, 0, 1 },
 		{ 1, 1, 1, 1, 1, 1, 1, 1 },
 	};
-
-	for (int j = 0; j < 15; j++) {
+	for (int j = 0; j < 19; j++) {
 		for (int i = 0; i < 8; i++) {
 			//mapの要素が1の時、四角形配置
 			if (map[j][i] == 1) {
@@ -62,6 +65,24 @@ void CSceneGame::Init() {
 				//右へ移動
 				Enemy->mFx = 0;
 				Enemy->mFy = -1;
+			}
+			else if (map[j][i] == 3){
+				CEnemy *Enemy = new CEnemy();
+				Enemy->x = i * 100 - 350;
+				Enemy->y = j *-100 + 250;
+				//右へ移動
+				Enemy->mFx = 0;
+				Enemy->mFy = -1;
+			}
+			else if (map[j][i] == 9){
+				CMap *Map = new CMap();
+				//四角形に値を設定
+				Map->mEnabled = true;
+				Map->x = i * 100 - 350;
+				Map->y = j*-100 + 350;
+				Map->w = 50;
+				Map->h = 50;
+				mxMax = Map->y + Map->h;
 			}
 		}
 	}
@@ -115,9 +136,15 @@ void CSceneGame::Update() {
 	//画面範囲右の設定
 	mRight = mLeft + 800.0;
 	//画面範囲下の設定
-	mBottm = CPlayer::spInstance->y - 550.0;
+	mBottm = CPlayer::spInstance->y - 350.0;
 	//画面範囲上の設定
 	mTop = mBottm + 800.0f;
+	if (mTop > mxMax)
+	{
+		float sa = mTop - mxMax;
+		mTop -= sa;
+		mBottm -= sa;
+	}
 	//画面投影範囲の変更
 	//行列をプロジェクションモードへ変更
 	glMatrixMode(GL_PROJECTION);
@@ -133,7 +160,6 @@ void CSceneGame::Update() {
 		VectorRect[i]->Render();
 	}
 }
-
 
 //次のシーンの取得
 CScene::EScene CSceneGame::GetNextScene() {
--- 2DLv1_99_Game/GameProgramming/CSceneGame.h
+++ 2DLv1_99_Game/GameProgramming/CSceneGame.h
@@ -13,7 +13,7 @@ public:
 	void Init();
 	//更新処理のオーバーライド
 	void Update();
-
+	float mxMax;
 	EScene GetNextScene();
 	//デストラクタの宣言
 	~CSceneGame();
